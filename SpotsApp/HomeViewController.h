93cfbcc92c3915c80eb1048066011cef0e4ae6b1
Andrey Poznyak <andrey.poznyak@viaden.com>
2014-04-28 01:09:17 +0300

    type of project changed, storyboard added

---
+++ SpotsApp/HomeViewController.h
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.h
+//  SpotsApp
+//
+//  Created by Andrey Poznyak on 28/04/14.
+//  Copyright (c) 2014 andreypoznyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+@property (nonatomic, strong) UILabel *myLabel;
+
+@end
