851b69ce97aa93cc45af0052223510d705ba5d30
Andrey Poznyak <andrey.poznyak@viaden.com>
2014-04-28 00:53:07 +0300

    Initial Commit

---
+++ SpotsApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpotsApp
+//
+//  Created by Andrey Poznyak on 28/04/14.
+//  Copyright (c) 2014 andreypoznyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpotsApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpotsApp
+//
+//  Created by Andrey Poznyak on 28/04/14.
+//  Copyright (c) 2014 andreypoznyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
