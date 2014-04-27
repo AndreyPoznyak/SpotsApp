e5e9c4e10b52264b31c92b8a750aefc66b0cf10e
Andrey Poznyak <andrey.poznyak@viaden.com>
2014-04-28 01:57:10 +0300

    testing view + table view

---
+++ SpotsApp/NetworksListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  NetworksListTableViewController.h
+//  SpotsApp
+//
+//  Created by Andrey Poznyak on 28/04/14.
+//  Copyright (c) 2014 andreypoznyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NetworksListTableViewController : UITableViewController
+
+@property (nonatomic, retain) NSArray *testArray;
+
+@end
